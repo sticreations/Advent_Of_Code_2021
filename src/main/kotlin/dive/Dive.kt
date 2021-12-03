438ec2c8f97b019db4bfdb8521c71a67069e58d3
Bastian Groß <bastian.gross@rewe-group.com>
2021-12-03 15:43:16 +0100

    Cleanup Diving

--- src/main/kotlin/dive/Dive.kt
+++ src/main/kotlin/dive/Dive.kt
@@ -2,48 +2,51 @@ package dive
 
 import util.getTestData
 
-fun divePartOne(command: List<Pair<String, Int>>): Int {
-    var x = 0
-    var y = 0
-    command.forEach {
+fun divePartOne(command: List<Pair<String, Int>>): Int =
+    command.map {
         when (it.first) {
-            "forward" -> {
-                x += it.second
-            }
-            "down" -> {
-                y += it.second
-            }
-            "up" -> {
-                y -= it.second
-            }
+            "forward" -> Pair(it.second, 0)
+            "down" -> Pair(0, it.second)
+            "up" -> Pair(0, -it.second)
+            else -> Pair(0, 0)
         }
-    }
-    return x * y
+    }.reduce { acc, pair -> Pair(acc.first + pair.first, acc.second + pair.second) }
+        .let { it.first * it.second }
+
+data class Dive(
+    var x: Int = 0,
+    var y: Int = 0,
+    var aim: Int = 0,
+)
+
+fun Dive.forward(i: Int) {
+    x += i; y += aim * i; }
+
+fun Dive.down(i: Int) {
+    aim += i
+}
+
+fun Dive.up(i: Int) {
+    aim -= i
 }
 
 fun divePartTwo(command: List<Pair<String, Int>>): Int {
-    var x = 0
-    var y = 0
-    var aim = 0
-    command.forEach {
-        when (it.first) {
-            "forward" -> {
-                x += it.second
-                y += aim * it.second
-            }
-            "down" -> {
-                aim += it.second
-            }
-            "up" -> {
-                aim -= it.second
+    with(Dive()) {
+        command.forEach {
+            when (it.first) {
+                "forward" -> this.forward(it.second)
+                "down" -> this.down(it.second)
+                "up" -> this.up(it.second)
             }
         }
+        return this.x*this.y
     }
-    return x * y
 }
 
+
 fun main() {
-    val testData = getTestData("input_02_dive.txt").map { Pair(it.substringBefore(" "), it.substringAfter(" ").toInt()) }
+    val testData =
+        getTestData("input_02_dive.txt").map { Pair(it.substringBefore(" "), it.substringAfter(" ").toInt()) }
     println("Day Two - Dive")
     println("Part One: ${divePartOne(testData)}")
     println("Part Two: ${divePartTwo(testData)}")
--- src/main/kotlin/sonar_sweep/SonarSweep.kt
+++ src/main/kotlin/sonar_sweep/SonarSweep.kt
@@ -4,7 +4,7 @@ import util.getTestData
 
 fun sonarSweep(sweeps: List<Int>): Int =sweeps.zipWithNext{a,b -> b-a}.count { it>0 }
 
-fun sonarSweepPartTwo(sweeps: List<Int>): Int = sonarSweep(sweeps.windowed(3,1, false) { (a,b,c) -> a+b+c })
+fun sonarSweepPartTwo(sweeps: List<Int>): Int = sonarSweep(sweeps.windowed(3,1) { (a,b,c) -> a+b+c })
 
 fun main() {
     val testData = getTestData("input_01_sonar_sweep.txt").map { it.toInt() }
